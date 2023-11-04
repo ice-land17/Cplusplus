b3faca439e553cb742cc4922227a2ec9d5943f8e
iceland017 <128897003+iceland017@users.noreply.github.com>
2023-11-04 21:57:27 +0800

    Add files via upload

---
+++ ChangeChar.java
@@ -0,0 +1,15 @@
+public class Boolean01
+{
+	public static void main(String[]args)
+        {
+            boolean IsPass=false;
+            if(IsPass == true)
+            {
+                System.out.println("考试通过");
+            }
+            else
+            {
+                System.out.println("考试没有通过");
+            }
+        }
+}
